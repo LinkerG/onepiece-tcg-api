3a525e5b9e40cc5dcb490b92ac1f44dd10ba339b
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2024-08-25 17:13:06 +0200

    auto documentacion GOD

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(process.env.PORT || 3000);
+
+    const config = new DocumentBuilder()
+        .setTitle('API Documentation')
+        .setDescription('The API description')
+        .setVersion('1.0')
+        .addTag('cards')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
+    await app.listen(3000);
 }
 bootstrap();
