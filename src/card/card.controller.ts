c0a1308158d03c1da9441133d30f8d8cf0dbcab7
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2024-08-29 11:04:24 +0200

    Cache cliente - servidor

--- src/card/card.controller.ts
+++ src/card/card.controller.ts
@@ -1,23 +1,30 @@
-import { Body, Controller, Get, Param, Patch, Post, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Header, Inject, Param, Patch, Post, Query, UseGuards, UseInterceptors } from '@nestjs/common';
 import { CardService } from './card.service';
 import { ApiBearerAuth, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 import { Card } from './schemas/card.schema';
 import { FindAllCardsQuery } from './dto/find-card.dto';
 import { CreateCardDto } from './dto/create-card.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { Cache, CACHE_MANAGER, CacheInterceptor } from '@nestjs/cache-manager';
 
 @Controller('card')
 @ApiTags('Card')
 export class CardController {
-    constructor(private readonly cardService: CardService) { }
+    constructor(
+        private readonly cardService: CardService,
+        @Inject(CACHE_MANAGER) private cacheManager: Cache) { }
 
     @Get()
+    @UseInterceptors(CacheInterceptor)
+    @Header('Cache-Control', 'public, max-age=300') // 5min
     @ApiOperation({ description: 'Get cards matching parameters' })
     async findAll(@Query() query: FindAllCardsQuery): Promise<Card[]> {
         return this.cardService.find(query);
     }
 
     @Get(":card_id")
+    @UseInterceptors(CacheInterceptor)
+    @Header('Cache-Control', 'public, max-age=300') // 5min
     @ApiParam({ name: 'card_id', description: 'ID of the card', type: String })
     @ApiOperation({ description: 'Get card by card_id' })
     async findById(@Param('card_id') card_id: string): Promise<Card> {
@@ -30,7 +37,9 @@ export class CardController {
     @ApiParam({ name: 'card_id', description: 'ID of the card', type: String })
     @ApiOperation({ description: 'Increases alternate art counter for card' })
     async addAlternateArt(@Param('card_id') card_id: string): Promise<Card> {
-        return this.cardService.addAlternateArt(card_id);
+        const result = this.cardService.addAlternateArt(card_id);
+        await this.cacheManager.del(`/card/${card_id}`)
+        return result
     }
 
     @Patch(":card_id/removeAlteredArt")
@@ -39,7 +48,9 @@ export class CardController {
     @ApiParam({ name: 'card_id', description: 'ID of the card', type: String })
     @ApiOperation({ description: 'Decreases alternate art counter for card' })
     async removeAlternateArt(@Param('card_id') card_id: string): Promise<Card> {
-        return this.cardService.removeAlternateArt(card_id);
+        const result = this.cardService.removeAlternateArt(card_id);
+        await this.cacheManager.del(`/card/${card_id}`)
+        return result
     }
 
     @Post()
@@ -47,6 +58,8 @@ export class CardController {
     @ApiBearerAuth()
     @ApiOperation({ description: 'Create a new card' })
     async create(@Body() card: CreateCardDto): Promise<Card> {
-        return this.cardService.create(card);
+        const result = this.cardService.create(card);
+        await this.cacheManager.del(`/card`)
+        return result;
     }
 }
--- src/card/card.module.ts
+++ src/card/card.module.ts
@@ -4,9 +4,14 @@ import { CardController } from './card.controller';
 import { CardService } from './card.service';
 import { Card, CardSchema } from './schemas/card.schema';
 import { AuthModule } from '../auth/auth.module';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
     imports: [
+        CacheModule.register({
+            ttl: 5000, // seconds
+            max: 100,
+        }),
         MongooseModule.forFeature(
             [{ name: Card.name, schema: CardSchema }]
         ),
